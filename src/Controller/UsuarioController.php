177c12fa2da89b561bd1fec048f5b641df3b1029
Alex <alexarau0015@hotmail.com>
2020-08-15 19:31:02 -0300

    Create class and routes with annotation for api

---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="web_usuario_")
+ */
+
+class UsuarioController
+{
+    /**
+     * @Route("/", methods={"GET"}, name="index")
+     */
+
+    public function index() : Response
+    {
+        return new Response("Implementar formulário de cadastro");            
+    }
+
+    /**
+     * @Route("/salvar", methods={"POST"}, name="salvar")
+     */
+
+     public function salvar() : Response
+     {
+         return new Response("Implementar gravação ao banco de dados");
+     }
+}
